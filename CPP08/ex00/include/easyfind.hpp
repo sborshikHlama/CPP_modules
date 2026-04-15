0320a5be22d7d9a11215daafc79031967f01b99f
Arsenii Evstigneev <aevstign@c1r1s5.42prague.com>
2026-04-15 18:27:22 +0200

    WIP

--- CPP08/ex00/include/easyfind.hpp
+++ CPP08/ex00/include/easyfind.hpp
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aevstign <aevstign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 18:25:40 by aevstign          #+#    #+#             */
+/*   Updated: 2026/04/15 18:26:30 by aevstign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
 #include <algorithm>
 #include <exception>
 #include <iterator>
+
 
 class ValueNotFoundException : public std::exception {
 public:
@@ -14,6 +27,9 @@ public:
 
 template <typename T>
 typename T::iterator easyfind(T &container, int value);
+
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value);
 
 #include "easyfind.tpp"
 
